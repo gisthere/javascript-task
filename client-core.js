77d0dd0e5c56fe70322b55e695224f6bcb209d0e
Fanis Kalimullin <fmkalimullin@gmail.com>
2018-03-26 17:44:06 +0300

    Solution by Fanis Kalimullin (gisthere)

--- client-core.js
+++ client-core.js
@@ -1,11 +1,114 @@
 'use strict';
+const commandLineArgs = require('command-line-args');
+const got = require('got');
+const chalk = require('chalk');
+const querystring = require('querystring');
 
 module.exports.execute = execute;
 module.exports.isStar = true;
 
 function execute() {
-    // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+    const commands = ['send', 'list'];
+
+    const args = parseCommandArgs();
+
+    if (!commands.includes(args.command)) {
+        return Promise.reject('Error');
+    }
+
+    if (args.command === 'send' && !args.text) {
+        return Promise.reject('Error');
+    }
+
+    const params = {};
+
+    if (args.from) {
+        params.from = args.from;
+    }
+
+    if (args.to) {
+        params.to = args.to;
+    }
+
+    if (args.command === 'send') {
+        params.text = args.text;
+
+        return sendMessage(params).then(message => {
+            return Promise.resolve(colorMessages([message]));
+        });
+    }
+
+    return getMessages(params).then(messages => {
+        return Promise.resolve(colorMessages(messages));
+    });
+}
+
+function parseCommandArgs() {
+
+    const mainDefinitions = [
+        { name: 'command', type: String, defaultOption: true },
+        { name: 'from', type: String },
+        { name: 'to', type: String },
+        { name: 'text', type: String }
+    ];
+
+    // filter empty params
+    let args = commandLineArgs(mainDefinitions, { partial: true });
+    args = Object.keys(args).filter(key => args[key])
+        .reduce((obj, key) => {
+            obj[key] = args[key];
+
+            return obj;
+        }, {});
+
+    return args;
+}
+
+function colorMessages(messages) {
+    const red = chalk.hex('#f00');
+    const green = chalk.hex('#0f0');
+    const result = messages.map(msg => {
+        let text = '';
+        if (msg.from) {
+            text = text.concat(`${red('FROM')}: ${msg.from}\n`);
+        }
+
+        if (msg.to) {
+            text = text.concat(`${red('TO')}: ${msg.to}\n`);
+        }
+
+        text = text.concat(`${green('TEXT')}: ${msg.text}`);
+
+        return text;
+    });
+
+    return result.join('\n\n');
+}
+
+function sendMessage(params) {
+    const query = {};
+    if (params.from) {
+        query.from = params.from;
+    }
+
+    if (params.to) {
+        query.to = params.to;
+    }
+    const body = { text: params.text };
+
+    return got.post('http://localhost:8080/messages/',
+        {
+            json: true,
+            query: querystring.stringify(query),
+            body: body
+        }).then(res => res.body);
+}
+
+function getMessages(params) {
+    return got.get('http://localhost:8080/messages/',
+        {
+            json: true,
+            query: querystring.stringify(params)
+        }).then(res => res.body);
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,75 @@
 'use strict';
 
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer();
+const app = express();
+const messages = [];
 
-server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+function getMessages(params) {
+    const filterParams = [];
+    if (params.from) {
+        filterParams.from = params.from;
+    }
+
+    if (params.to) {
+        filterParams.to = params.to;
+    }
+
+    return filterMessages(filterParams);
+}
+
+function postMessage(params) {
+    const msg = { text: params.text };
+    if (params.from) {
+        msg.from = params.from;
+    }
+
+    if (params.to) {
+        msg.to = params.to;
+    }
+
+    messages.push(msg);
+
+    return msg;
+}
+
+function filterMessages(filterParams) {
+    const filtered = messages.filter(message => {
+        return Object.keys(filterParams).every(key => {
+            return message.hasOwnProperty(key) && message[key] === filterParams[key];
+        });
+    });
+
+    return filtered;
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/messages/?', (req, res) => {
+    const params = req.query;
+    const msgs = getMessages({ from: params.from, to: params.to });
+    res.send(msgs);
 });
 
-module.exports = server;
+app.post('/messages/?', (req, res) => {
+    if (!req.body.text) {
+        res.sendStatus(400);
+
+        return;
+    }
+    const params = req.query;
+    const msg = postMessage({ from: params.from, to: params.to, text: req.body.text });
+    res.send(msg);
+});
+
+app.all('*', (req, res) => {
+    res.sendStatus(404);
+});
+
+module.exports = app;
+
+
